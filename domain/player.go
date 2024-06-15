8c96372a9128db5168dc410c97df4d2f2cbca5b8
pikachu0310 <pikachu13711@gmail.com>
2024-06-16 00:49:32 +0900

    :sparkles: サーバーで同期するパラメーターを大幅に増やした

--- domain/player.go
+++ domain/player.go
@@ -1,13 +1,43 @@
 package domain
 
-type PlayerData struct {
-	ID       string  `json:"id"`
-	Position Vector3 `json:"position"`
-	Speed    float32 `json:"speed"`
-}
-
 type Vector3 struct {
 	X float32 `json:"x"`
 	Y float32 `json:"y"`
 	Z float32 `json:"z"`
+}
+
+type PlayerData struct {
+	Type       string  `json:"type"`
+	ID         string  `json:"id"`
+	Name       string  `json:"name"`
+	Position   Vector3 `json:"position"`
+	Speed      Vector3 `json:"speed"`
+	MaxHP      float32 `json:"maxHP"`
+	HP         float32 `json:"hp"`
+	Mass       float32 `json:"mass"`
+	Bounciness float32 `json:"bounciness"`
+	Friction   float32 `json:"friction"`
+	Size       float32 `json:"size"`
+	Score      int     `json:"score"`
+	GrabTarget string  `json:"grabTarget"`
+	GrabLength float32 `json:"grabLength"`
+}
+
+type ItemData struct {
+	Type        string `json:"type"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Attribute   int    `json:"attribute"`
+	Rarity      int    `json:"rarity"`
+}
+
+type BulletData struct {
+	Type          string  `json:"type"`
+	ID            string  `json:"id"`
+	FirstPosition Vector3 `json:"firstPosition"`
+	FirstSpeed    Vector3 `json:"firstSpeed"`
+	RemainingTime float32 `json:"remainingTime"`
+	Attack        float32 `json:"attack"`
+	Size          float32 `json:"size"`
 }
--- server/client.go
+++ server/client.go
@@ -19,6 +19,8 @@ type Client struct {
 
 var clients = make(map[*Client]bool)
 var players = make(map[string]domain.PlayerData)
+var items = make(map[string]domain.ItemData)
+var bullets = make(map[string]domain.BulletData)
 var mutex = &sync.Mutex{}
 
 func NewClient(ws *websocket.Conn) *Client {
@@ -69,15 +71,43 @@ func (client *Client) readLoop() {
 		}
 		fmt.Println("[RECEIVE] " + string(data))
 
-		var playerData domain.PlayerData
-		if err := json.Unmarshal(data, &playerData); err != nil {
-			log.Printf("Error unmarshalling player data: %v", err)
+		var base struct {
+			Type string `json:"type"`
+		}
+		if err := json.Unmarshal(data, &base); err != nil {
+			log.Printf("Error unmarshalling base data: %v", err)
 			continue
 		}
 
-		mutex.Lock()
-		players[playerData.ID] = playerData
-		mutex.Unlock()
+		switch base.Type {
+		case "player":
+			var playerData domain.PlayerData
+			if err := json.Unmarshal(data, &playerData); err != nil {
+				log.Printf("Error unmarshalling player data: %v", err)
+				continue
+			}
+			mutex.Lock()
+			players[playerData.ID] = playerData
+			mutex.Unlock()
+		case "item":
+			var itemData domain.ItemData
+			if err := json.Unmarshal(data, &itemData); err != nil {
+				log.Printf("Error unmarshalling item data: %v", err)
+				continue
+			}
+			mutex.Lock()
+			items[itemData.ID] = itemData
+			mutex.Unlock()
+		case "bullet":
+			var bulletData domain.BulletData
+			if err := json.Unmarshal(data, &bulletData); err != nil {
+				log.Printf("Error unmarshalling bullet data: %v", err)
+				continue
+			}
+			mutex.Lock()
+			bullets[bulletData.ID] = bulletData
+			mutex.Unlock()
+		}
 	}
 }
 
@@ -106,17 +136,39 @@ func handleMessages() {
 		for _, player := range players {
 			playerList = append(playerList, player)
 		}
-		mutex.Unlock()
-
 		playerDataJSON, err := json.Marshal(playerList)
 		if err != nil {
 			log.Printf("Error marshalling player data: %v", err)
+			mutex.Unlock()
 			continue
 		}
 
-		mutex.Lock()
+		itemList := make([]domain.ItemData, 0, len(items))
+		for _, item := range items {
+			itemList = append(itemList, item)
+		}
+		itemDataJSON, err := json.Marshal(itemList)
+		if err != nil {
+			log.Printf("Error marshalling item data: %v", err)
+			mutex.Unlock()
+			continue
+		}
+
+		bulletList := make([]domain.BulletData, 0, len(bullets))
+		for _, bullet := range bullets {
+			bulletList = append(bulletList, bullet)
+		}
+		bulletDataJSON, err := json.Marshal(bulletList)
+		if err != nil {
+			log.Printf("Error marshalling bullet data: %v", err)
+			mutex.Unlock()
+			continue
+		}
+
 		for client := range clients {
 			client.send <- playerDataJSON
+			client.send <- itemDataJSON
+			client.send <- bulletDataJSON
 		}
 		mutex.Unlock()
 	}
